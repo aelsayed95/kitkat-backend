871c1196ecb910f11d23d465395a1cf02b26ac63
Aya <ayah.ehab11@gmail.com>
2022-09-25 12:43:35 +0100

    update endpoints

--- server.py
+++ server.py
@@ -9,22 +9,30 @@ kudos = [
     "thanks @marios for help with #cmake",
 ]
 
+members_list = ["Aya", "Alex", "Marios"]
+
 @app.route("/members")
 def members():
-    members = ["Aya", "Alex", "Marios"]
-    return {"members": members}
+    return {"members": members_list}
 
+@app.route("/members/add", methods=['POST'])
+def member_add():
+    name = request.get_json().get('name')
+    print(name)
+    print("in hereereee")
+    members_list.append(name)
+    return {"members": members_list}
 
-@app.route("/recent-kudos")
+@app.route("/kudos")
 def recent_kudos():
     return {"kudos": kudos}
 
-@app.route("/add-kudos", methods=['POST'])
+@app.route("/kudos/add", methods=['POST'])
 def add_kudos():
     message = request.get_json().get('kudos')
     logging.info("received add-kudos request with message: {message}")
     kudos.append(message)
-    return {}
+    return {"kudos": kudos}
 
 
 if __name__ == "__main__":
